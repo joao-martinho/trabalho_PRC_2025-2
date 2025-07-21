70ea5713e35aa4024a2966dac70133b31aa1df14
Cristina SJ <sjcristina9@gmail.com>
2025-07-21 00:57:26 -0300

    :sparkles: feat: Sessão com id e senha, páginas

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
-import socket
+from flask import Flask, request, render_template, session, redirect, url_for
+import socket, re
 
 app = Flask(__name__)
+app.secret_key = 'test'
 
 TCP_HOST = 'larc.furb.br'
 TCP_PORT = 1012
@@ -24,29 +25,70 @@ def udp_send(message):
             return "Mensagem enviada com sucesso"
     except Exception as e:
         return f"Erro UDP: {str(e)}"
+  
+  
+@app.route('/')
+def frontpage():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('messages_dashboard'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        form_user_id = request.form['user_id']
+        form_password = request.form['password']
+        
+        valid_user_id = re.fullmatch(r'\d{4}', form_user_id)
+        valid_password = re.fullmatch(r'[a-z]{5}', form_password)
+
+        if valid_user_id and valid_password:
+            session['user_id'] = form_user_id
+            session['password'] = form_password
+            return redirect(url_for('messages_dashboard'))
+        else:
+            return render_template('login.html', error='Usuário ou senha inválidos')
+
+    return render_template('login.html')
+
+@app.route('/messages', methods = ['GET', 'POST'])
+def messages_dashboard():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    
+    if request.method == 'POST':
+        return redirect(url_for('send_message'))
+
+    return render_template('messages.html', logged_user=session['user_id'])
 
 @app.route('/get-users')
 def get_users():
-    user = request.args.get('user')
-    password = request.args.get('password')
-    msg = f"GET USERS {user}:{password}"
+    if 'user_id' not in session: return 'Usuário não autenticado', 401
+    
+    msg = f"GET USERS {session['user_id']}:{session['password']}"
     return tcp_request(msg)
 
 @app.route('/get-message')
 def get_message():
-    user = request.args.get('user')
-    password = request.args.get('password')
-    msg = f"GET MESSAGE {user}:{password}"
+    if 'user_id' not in session: return 'Usuário não autenticado', 401
+    
+    msg = f"GET MESSAGE {session['user_id']}:{session['password']}"
     return tcp_request(msg)
 
 @app.route('/send-message')
 def send_message():
-    from_id = request.args.get('from')
-    password = request.args.get('pass')
-    to_id = request.args.get('to')
-    msg_text = request.args.get('msg')
-    msg = f"SEND MESSAGE {from_id}:{password}:{to_id}:{msg_text}"
+    if 'user_id' not in session: return 'Usuário não autenticado', 401
+    
+    to_id = request.args.get('to_id')
+    msg_text = request.args.get('msg_text')
+    msg = f"SEND MESSAGE {session['user_id']}:{session['password']}:{to_id}:{msg_text}"
     return udp_send(msg)
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=5000)
